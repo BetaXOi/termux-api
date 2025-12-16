742026adc4b11a30c0887a4181f9b73ef6420435
ctyun <ctyun@prod-arm-deb-builder01>
2025-12-16 11:35:08 +0800

    change version

--- app/src/main/java/com/termux/api/TermuxApiReceiver.java
+++ app/src/main/java/com/termux/api/TermuxApiReceiver.java
@@ -8,7 +8,7 @@ import android.content.Intent;
 import android.provider.Settings;
 import android.widget.Toast;
 
-import com.termux.api.apis.AdbWifiAPI
+import com.termux.api.apis.AdbWifiAPI;
 import com.termux.api.apis.AudioAPI;
 import com.termux.api.apis.BatteryStatusAPI;
 import com.termux.api.apis.BrightnessAPI;
